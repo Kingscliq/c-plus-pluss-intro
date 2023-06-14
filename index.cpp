f86370d80034f498311161eab20950d60cdc4c5c
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-06-14 17:26:28 +0100

    initial commit for fist c++ code

---
+++ index.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main (){
+    std::cout << "Hello World!";
+
+    return 0;
+}
+
+
+/**
+ * 
+ * Data Types
+ * Variables 
+ * functions
+ * loops
+ * Keywords
+ * High order methods
+ * Objects
+ * Arrays
+ * Logging
+ * Operations
+ * Control flow statements(if, switch statements  etc...)
+ * */ 
