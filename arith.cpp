a2647f2c66ca690c746df74882861923ea14cdfc
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-06-23 06:18:40 +0100

    ci: update arithmetic operations in maths

--- arith.cpp
+++ arith.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
     int num1 = 30;
     int num2 = 20;
     int sum = num1 + num2;
-    std::cout << sum << std::endl;
+
+    // Arithmetic operations
+    // cout << sum << endl;
+
+    double x = 10;
+    int y = 5;
+
+    double z = (x + 10) / (3 * y);
+
+    cout << z << endl;
 }
