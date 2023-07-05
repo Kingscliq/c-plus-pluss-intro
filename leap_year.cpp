859b2df6238e3506389efa8fe7333c2471c362fc
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-07-05 22:09:57 +0100

    update: leap year code implementation

---
+++ leap_year.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int year;
+    cout << "Enter year ";
+    cin >> year;
+
+    if (year % 400 == 0)
+    {
+        cout << "This is a leap year" << endl;
+    }
+    else if (year % 100 == 0)
+    {
+        cout << "This is not a leap year" << endl;
+    }
+    else if (year % 4 == 0)
+    {
+        cout << "This is a leap year" << endl;
+    }
+    else
+    {
+        cout << "This is not a leap year" << endl;
+    }
+}
